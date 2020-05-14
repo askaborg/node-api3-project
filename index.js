2437bf2d47fd42860672c236ae0c99661122c46c
hienle <hienle@askaborg.com>
2020-05-14 14:42:37 -0700

    node start

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
+require("dotenv").config()
+
 const server = require("./server.js")
 
-server.listen(5000, () => {
-  console.log("\n* Listening on http://localhost:5000 *\n")
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+  console.log(`\n* Listening on http://localhost:${port} *\n`)
 })
